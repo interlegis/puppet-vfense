ff28f74f3102d211aa7e63e5011a61c384d0b76c
Fabio Rauber <fabiorauber@gmail.com>
2014-08-13 09:29:30 -0300

    Added option to remove vFense

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -2,46 +2,69 @@
 #Requires: https://github.com/example42/puppet-wget
 
 class vfense::agent (
-	$source = 'http://www.vfense.org/download/vfense-debian-agent/',
+	$source = 'https://github.com/vFense/vFenseAgent-nix/releases/download/v0.7.2/VFAgent_0_7_2-deb.tar.gz',
         $user,
         $password,
         $servername,
         $tag = 'default',
+        $ensure = 'installed',
  ) {
-
-  validate_string($user)
-  validate_string($password)
-  validate_string($servername)
   
-  wget::fetch { "vfense-agent":
-    source => $source,
-    destination => '/tmp/vfense.tar.gz',
-    notify => Exec['unpack vfense-agent'],
-  } 
+  case $ensure {
+    'installed': {
+   
+      validate_string($user)
+      validate_string($password)
+      validate_string($servername)
+   
+      wget::fetch { "vfense-agent":
+        source => $source,
+        destination => '/tmp/vfense.tar.gz',
+        notify => Exec['unpack vfense-agent'],
+      } 
 
-  file { '/tmp/vfense-agent':
-    ensure => directory,
-  }
+      file { '/tmp/vfense-agent':
+        ensure => directory,
+      }
  
-  exec { 'unpack vfense-agent':
-    cwd => '/tmp',
-    command => '/bin/tar -xvzf /tmp/vfense.tar.gz -C /tmp/vfense-agent --strip-components=1',
-    creates => '/tmp/vfense-agent/agent',
-    require => File['/tmp/vfense-agent'],
-    notify => Exec['vfense-agent install'],
-  }
+      exec { 'unpack vfense-agent':
+        cwd => '/tmp',
+        command => '/bin/tar -xvzf /tmp/vfense.tar.gz -C /tmp/vfense-agent --strip-components=1',
+        creates => '/tmp/vfense-agent/agent',
+        require => File['/tmp/vfense-agent'],
+        notify => Exec['vfense-agent install'],
+      }
  
-  exec { 'vfense-agent install':
-    cwd => '/tmp/vfense-agent',
-    command => "./install -u '$user' -p '$password' -s '$servername' -c '$tag'",
-    creates => '/opt/TopPatch',
-  }
- 
-  service { 'tpagentd':
-    ensure => running,
-    enable => true,
-    status => '/usr/bin/test -e /tmp/toppatch_agent.pid',
-    require => Exec["vfense-agent install"],
-  } 
+      exec { 'vfense-agent install':
+        cwd => '/tmp/vfense-agent',
+        command => "./install -u '$user' -p '$password' -s '$servername' -c '$tag'",
+        creates => '/opt/TopPatch',
+      }
+      service { 'tpagentd':
+        ensure => running,
+        enable => true,
+        status => '/usr/bin/test -e /tmp/toppatch_agent.pid',
+        require => Exec["vfense-agent install"],
+      }
+    }
+    'absent': {
+      file { '/tmp/vfense-agent':
+        ensure => 'absent',
+        force => true,
+        require => Service['tpagentd'],
+      }
+      file { '/opt/TopPatch':
+        recurse => true,
+        force => true,
+        ensure  => 'absent',
+        require => Service['tpagentd'],
+      }
+      service { 'tpagentd':
+        ensure => stopped,
+        enable => false,
+        status => '/usr/bin/test -e /tmp/toppatch_agent.pid',
+      }
 
+    }
+  }
 }
